1343f11b3102b8ae6535525052188dd67a10b00f
Julian Ramiro Sanchez <julian.ramiro@gmail.com>
2024-10-10 14:14:53 +0200

    Front: Register componente created.

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/app-routing.module.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from 'ontimize-web-ngx';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   { path: 'main', canActivate: [AuthGuardService], loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'register', component: RegisterComponent},
   { path: '**', redirectTo: 'main', pathMatch: 'full' },
   { path: '', redirectTo: 'main', pathMatch: 'full' }
 ];
--- cd2024bfs5g1-frontend/src/main/ngx/src/app/app.module.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { CONFIG } from './app.config';
 import { MainService } from './shared/services/main.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './register/register.component';
 
 // Standard providers...
 // Defining custom providers (if needed)...
@@ -29,7 +30,8 @@ export const customProviders: any = [
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   bootstrap: [
     AppComponent

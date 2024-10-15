9aa32e3a7ce097669e6699094dfdb83903df1546
AGonzL <agonzlopez.11@gmail.com>
2024-10-15 09:50:40 +0200

    Tablas del formulario actualizado con css y logica de insert

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings-new/coworkings-new.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings-new/coworkings-new.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { OFormComponent, DialogService } from 'ontimize-web-ngx';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-coworkings-new',
+  selector: 'app-coworking-new',
   templateUrl: './coworkings-new.component.html',
   styleUrls: ['./coworkings-new.component.css']
 })
 export class CoworkingsNewComponent {
+
+  @ViewChild('coworkingForm') coworkingForm: OFormComponent;
+
+  constructor(
+    private router: Router,
+    private dialogService: DialogService
+  ) {}
+
+  public onInsertSuccess(): void {
+
+    this.coworkingForm.setInitialMode();
+
+    //const coworkingId = data.id;
+
+
+    this.dialogService.info('Operación exitosa', 'El coworking se ha guardado correctamente');
+  }
 
 }

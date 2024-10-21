38bed8e3fae7132f47fab6fb4c924615acfabd29
César Miguel Santiago Núñez <cmiguel.santiago@imatia.com>
2024-10-21 17:00:29 +0200

    corrected foreign key

--- cd2024bfs5g1-model/src/main/resources/db/migration/V2__create_table_coworkings.sql
+++ cd2024bfs5g1-model/src/main/resources/db/migration/V2__create_table_coworkings.sql
@@ -9,5 +9,5 @@ CREATE TABLE coworking (
     cw_usr_id INT,
     cw_start_date DATE default CURRENT_DATE,
     cw_end_date DATE
-CONSTRAINT cw_usr_id_fk FOREIGN KEY (cw_usr_id) REFERENCES usr_user(usr_id)
+CONSTRAINT cw_usr_id_fk FOREIGN KEY(cw_usr_id) REFERENCES usr_user(usr_id)
 );

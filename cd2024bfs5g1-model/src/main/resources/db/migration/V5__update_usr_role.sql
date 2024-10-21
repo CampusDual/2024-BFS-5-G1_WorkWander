257e44d5931bd4c455df4f1ef9d632b99ce5bf5d
César Miguel Santiago Núñez <cmiguel.santiago@imatia.com>
2024-10-21 17:19:53 +0200

    corrected update

--- cd2024bfs5g1-model/src/main/resources/db/migration/V5__update_usr_role.sql
+++ cd2024bfs5g1-model/src/main/resources/db/migration/V5__update_usr_role.sql
@@ -1,4 +1,4 @@
-update usr_role set rol_json_client_permission = {"menu":[{"attr":"coworkingsPublic","visible":false,"enabled":false},{"attr":"login_public","visible":false,"enabled":false}],"routes":[{"permissionId":"coworkingsPublic","enabled":false},{"permissionId":"login_public","enabled":false}]} where
+update usr_role set rol_json_client_permission = '{"menu":[{"attr":"coworkingsPublic","visible":false,"enabled":false},{"attr":"login_public","visible":false,"enabled":false}],"routes":[{"permissionId":"coworkingsPublic","enabled":false},{"permissionId":"login_public","enabled":false}]}' where
 rol_id = 1;
 update usr_role set rol_json_client_permission = '{"menu":[{"attr":"admin","visible":false,"enabled":false},
 {"attr":"MyCoworkings","visible":false,"enabled":false},{"attr":"newEvents","visible":false,"enabled":false},

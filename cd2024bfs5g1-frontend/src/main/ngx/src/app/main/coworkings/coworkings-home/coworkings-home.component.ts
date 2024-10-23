e29e63ed30641ac50cfd709bd85363ea4586cf4b
AGonzL <agonzlopez.11@gmail.com>
2024-10-24 00:12:09 +0200

    Actualización de estilos coworking-home aproximandonos a lo que se busca - falta mejorar el responsive - posteriormente podemos plantearnos pasar la ficha a un componente para reutilización.

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings-home/coworkings-home.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings-home/coworkings-home.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-coworkings-home',
   templateUrl: './coworkings-home.component.html',
-  styleUrls: ['./coworkings-home.component.css']
+  styleUrls: ['./coworkings-home.component.scss']
 })
 export class CoworkingsHomeComponent {
-
-
+  // Función que retorna una URL de imagen de prueba basada en el ID del coworking o una imagen fija
+  getImageUrl(coworkingId: number): string {
+    // Puedes usar cualquier URL de imagen para pruebas, aquí utilizo Lorem Picsum
+    return `https://picsum.photos/seed/${coworkingId}/300/200`;
+  }
 }
+

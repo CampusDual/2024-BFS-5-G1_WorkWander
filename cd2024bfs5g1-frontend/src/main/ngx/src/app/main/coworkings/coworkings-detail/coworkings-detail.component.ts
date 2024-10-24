ac10c883a8d43c6dc79eb55d5c45172599dd1e40
Diego Miras Curras <diegomiras93@gmail.com>
2024-10-24 10:50:24 +0200

    Creation of method checkCapacity with the service to consult the number of bookings to the back. And addition of the endpoint /bookings to app.service.config

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings-detail/coworkings-detail.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings-detail/coworkings-detail.component.ts
@@ -9,9 +9,29 @@ import { OntimizeService } from "ontimize-web-ngx";
   styleUrls: ["./coworkings-detail.component.css"],
 })
 export class CoworkingsDetailComponent {
+  constructor(private service: OntimizeService) {}
 
-  
+  dateSelected: Date;
+  realCapacity: number = 100;
+
   currentDate() {
     return new Date();
+  }
+
+  checkCapacity(data) {
+    const filter = {
+      bk_cw_id: data["cw_id"],
+      bk_date: data["date"],
+    };
+    const conf = this.service.getDefaultServiceConfiguration("bookings");
+    this.service.configureService(conf);
+    const columns = ["bk_id"];
+    this.service.query(filter, columns, "bookingsByDate").subscribe((resp) => {
+      if (resp.code === 0) {
+        // resp.data contains the data retrieved from the server
+      } else {
+        alert("Impossible to query data!");
+      }
+    });
   }
 }

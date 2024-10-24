f05a7d8dc45f0ea5f3871a87b88317f425f8ea72
David Acha Oliveira <125604583+davidacha@users.noreply.github.com>
2024-10-24 09:46:29 +0200

    Corregida inserción de permisos para usuarios y mejora en la inserción de permisos en la base de datos

--- cd2024bfs5g1-model/src/main/resources/db/migration/V7__update_role_server_permissions_and_usr_role.sql
+++ cd2024bfs5g1-model/src/main/resources/db/migration/V7__update_role_server_permissions_and_usr_role.sql
@@ -1,13 +1,11 @@
 UPDATE usr_role
 SET rol_json_client_permission = '{"menu":[{"attr":"admin","visible":false,"enabled":false},
-{"attr":"MyCoworkings","visible":false,"enabled":false},
-{"attr":"coworkingsPublic","visible":false,"enabled":false},
-{"attr":"login_public","visible":false,"enabled":false}],
-"routes":[{"permissionId":"myCoworkings","enabled":false},
-{"permissionId":"coworkingsPublic","enabled":false},{"permissionId":"login_public","enabled":false}]}'
+{"attr":"MyCoworkings","visible":false,"enabled":false},{"attr":"coworkingsPublic","visible":false,"enabled":false},
+{"attr":"login_public","visible":false,"enabled":false},{"attr":"coworkings","visible":false,"enabled":false}],
+"routes":[{"permissionId":"myCoworkings","enabled":false},{"permissionId":"coworkingsPublic","enabled":false},
+{"permissionId":"login_public","enabled":false},{"permissionId":"coworkings-new.route","enabled":false}]}'
 WHERE rol_id = 2;
 
-insert into usr_role_server_permission (rol_id,srp_id)
-values
-	(2,19),
-	(2,20);
+insert into usr_role_server_permission (rol_id,srp_id) values
+(2,(select srp_id from usr_server_permission where srp_name = 'com.campusdual.cd2024bfs5g1.api.core.service.IEventService/eventInsert')),
+(2,(select srp_id from usr_server_permission where srp_name = 'com.campusdual.cd2024bfs5g1.api.core.service.IEventService/eventQuery'));

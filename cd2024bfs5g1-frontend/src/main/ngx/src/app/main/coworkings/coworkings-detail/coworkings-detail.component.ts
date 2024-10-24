ee723c74143aae9acbdaa9c79006e1647be338c2
Diego Miras Curras <diegomiras93@gmail.com>
2024-10-24 09:10:56 +0200

    Date selector added to Coworking Detail

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings-detail/coworkings-detail.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings-detail/coworkings-detail.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import moment from "moment";
+import { OntimizeService } from "ontimize-web-ngx";
 
 @Component({
-  selector: 'app-coworkings-detail',
-  templateUrl: './coworkings-detail.component.html',
-  styleUrls: ['./coworkings-detail.component.css']
+  selector: "app-coworkings-detail",
+  templateUrl: "./coworkings-detail.component.html",
+  styleUrls: ["./coworkings-detail.component.css"],
 })
 export class CoworkingsDetailComponent {
+
   
+  currentDate() {
+    return new Date();
+  }
 }

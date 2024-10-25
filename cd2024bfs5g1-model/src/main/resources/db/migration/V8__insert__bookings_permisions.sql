00fc107577c0ab4253a8ffeaa81307d4bb754a71
AdelaSRuizDeCortázar <sanruiade@gmail.com>
2024-10-25 12:46:12 +0200

    Fixed permissions

--- cd2024bfs5g1-model/src/main/resources/db/migration/V8__insert__bookings_permisions.sql
+++ cd2024bfs5g1-model/src/main/resources/db/migration/V8__insert__bookings_permisions.sql
@@ -2,7 +2,3 @@
 insert into usr_server_permission (srp_name) values ('com.campusdual.cd2024bfs5g1.api.core.service.IBookingService/bookingInsert');
 
 insert into usr_role_server_permission (rol_id, srp_id) values (2, (select srp_id from usr_server_permission where srp_name = 'com.campusdual.cd2024bfs5g1.api.core.service.IBookingService/bookingInsert'));
-
-insert into usr_server_permission (srp_name) values ('com.campusdual.cd2024bfs5g1.api.core.service.IBookingService/bookingQuery');
-
-insert into usr_role_server_permission (rol_id, srp_id) values (2, (select srp_id from usr_server_permission where srp_name = 'com.campusdual.cd2024bfs5g1.api.core.service.IBookingService/bookingQuery'));

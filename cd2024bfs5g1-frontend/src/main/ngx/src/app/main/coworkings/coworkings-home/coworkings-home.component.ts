208d3eaa65bead1312a8aec87d760a29c514d20e
Miguel Ángel Rama Martínez <mianramar@gmail.com>
2024-10-28 14:09:14 +0100

    Código comentado
    - Cambio de script a V11
    
    - Corrección de comentarios necesarios

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings-home/coworkings-home.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings-home/coworkings-home.component.ts
@@ -9,31 +9,35 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class CoworkingsHomeComponent implements OnInit{
 
+  // Creamos constructor
   constructor(
     protected dialog: MatDialog,
     protected sanitizer: DomSanitizer
   ) {}
 
+  // Creamos una variable para pasarle al html el número de columnas, por defecto 2
   public gridCols: number = 2;
 
   ngOnInit() {
+    // Al cargar, obtendremos al ancho de pantalla, para posteriormente pasarselo como parámetro a la funcion setGridCols
     this.setGridCols(window.innerWidth);
   }
 
+  // Función que cambiará el número de columnas a 1 si el ancho de ventana es menor de 1000
+  setGridCols(width: number) {
+    this.gridCols = width < 1000 ? 1 : 2;
+  }
+
+  // Listener para que cuando se cambie el tamaño de la ventana, llamar al evento y la funcion setGridCols
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
     this.setGridCols((event.target as Window).innerWidth);
   }
 
-  setGridCols(width: number) {
-    this.gridCols = width < 1000 ? 1 : 2;
-  }
-
+  // Función para convertir la imagen desde la base de datos
   public getImageSrc(base64: any): any {
     console.log(base64);
     return base64 ? this.sanitizer.bypassSecurityTrustResourceUrl('data:image/*;base64,' + base64) : './assets/images/coworking-default.jfif';
-    
-    // return base64 ? this.sanitizer.bypassSecurityTrustResourceUrl('data:image/*;base64,' + base64.bytes) : './assets/images/coworking-default.jfif';
   }
 
 }

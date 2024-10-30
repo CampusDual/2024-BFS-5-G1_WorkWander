b0350147e6b14f624cf2d5564339f62d3bb8873c
David Acha Oliveira <125604583+davidacha@users.noreply.github.com>
2024-10-30 09:34:52 +0100

    Usuario sin registrar no puede acceder a profile

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/settings/settings-routing.module.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/settings/settings-routing.module.ts
@@ -10,9 +10,30 @@ const routes: Routes = [
   {
     path: '', component: SettingsComponent, children: [
       { path: '', redirectTo:'profile' },
-      { path: 'profile', component: SettingsProfileComponent },
-      { path: 'account', component: SettingsAccountComponent },
-      { path: 'appearance', component: SettingsAppearanceComponent }
+      { path: 'profile', component: SettingsProfileComponent,
+        data: {
+          oPermission: {
+            permissionId: "profile",
+            restrictedPermissionsRedirect: "403",
+          },
+        },
+       },
+      { path: 'account', component: SettingsAccountComponent, 
+        data: {
+          oPermission: {
+            permissionId: "account",
+            restrictedPermissionsRedirect: "403",
+          },
+        },
+      },
+      { path: 'appearance', component: SettingsAppearanceComponent,
+        data: {
+          oPermission: {
+            permissionId: "appearance",
+            restrictedPermissionsRedirect: "403",
+          },
+        },
+       }
     ]
   }
 ];

83b234a594e20362e0d63b3a5174cbd2900e6d31
JuanDAW37 <jymtr1968@gmail.com>
2024-11-04 10:49:21 +0100

    bug: Eliminación de la dependencia no usada.

--- cd2024bfs5g1-api/src/main/java/com/campusdual/cd2024bfs5g1/api/core/service/ICoworkingService.java
+++ cd2024bfs5g1-api/src/main/java/com/campusdual/cd2024bfs5g1/api/core/service/ICoworkingService.java
@@ -1,7 +1,6 @@
 package com.campusdual.cd2024bfs5g1.api.core.service;
 
 import com.ontimize.jee.common.dto.EntityResult;
-import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 
 import java.util.List;
 import java.util.Map;

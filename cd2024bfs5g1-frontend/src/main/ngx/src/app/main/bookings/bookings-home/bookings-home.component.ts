53360116e947f45b32abd12031137d01cad9537b
Diego Miras Curras <diegomiras93@gmail.com>
2024-11-12 14:34:04 +0100

    Editado html bookings home , traducciones, y metodo para fecha incial y final

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/bookings/bookings-home/bookings-home.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/bookings/bookings-home/bookings-home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { ODateInputComponent, OTableColumn, OTableColumnComponent } from "ontimize-web-ngx";
 
 @Component({
-  selector: 'app-bookings-home',
-  templateUrl: './bookings-home.component.html',
-  styleUrls: ['./bookings-home.component.css']
+  selector: "app-bookings-home",
+  templateUrl: "./bookings-home.component.html",
+  styleUrls: ["./bookings-home.component.css"],
 })
 export class BookingsHomeComponent {
+
+
+
+  @ViewChild("dates") bookingDate: OTableColumnComponent;
+
+  private arrayDates: OTableColumnComponent;
+  private dates: [];
+
+  showStartDate(){
+    return this.arrayDates[0];
+  }
+  showEndDate(){
+    return this.arrayDates[length-1];
+  }
 
 }

deb669cd5d0e6a24d9e12cd13b6fd6195a05cc56
Beatriz Faro Carrera <farocarrera.beatriz@gmail.com>
2024-11-13 14:46:26 +0100

    Corrección formato fecha y hora events-new.

--- cd2024bfs5g1-model/src/main/java/com/campusdual/cd2024bfs5g1/model/core/service/EventService.java
+++ cd2024bfs5g1-model/src/main/java/com/campusdual/cd2024bfs5g1/model/core/service/EventService.java
@@ -1,7 +1,6 @@
 package com.campusdual.cd2024bfs5g1.model.core.service;
 
 import com.campusdual.cd2024bfs5g1.api.core.service.IEventService;
-import com.campusdual.cd2024bfs5g1.model.core.dao.CoworkingDao;
 import com.campusdual.cd2024bfs5g1.model.core.dao.EventDao;
 import com.campusdual.cd2024bfs5g1.model.core.dao.UserDao;
 import com.ontimize.jee.common.dto.EntityResult;
@@ -16,6 +15,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -30,27 +32,49 @@ public class EventService implements IEventService {
     private DefaultOntimizeDaoHelper daoHelper;
 
     @Override
-    @Secured({ PermissionsProviderSecured.SECURED })
+    @Secured({PermissionsProviderSecured.SECURED})
     @Transactional(rollbackFor = Exception.class)
-    public EntityResult eventQuery(Map<String, Object> keyMap, List<String> attrList) {
+    public EntityResult eventQuery(final Map<String, Object> keyMap, final List<String> attrList) {
         return this.daoHelper.query(this.eventDao, keyMap, attrList);
     }
 
     @Override
-    @Secured({ PermissionsProviderSecured.SECURED })
+    @Secured({PermissionsProviderSecured.SECURED})
     @Transactional(rollbackFor = Exception.class)
-    public EntityResult eventInsert(Map<String, Object> attributes) throws OntimizeJEERuntimeException {
-        Object user = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        int userId = (int)((UserInformation) user).getOtherData().get(UserDao.USR_ID);
-        attributes.put(EventDao.USR_ID ,userId);
+    public EntityResult eventInsert(final Map<String, Object> attributes) throws OntimizeJEERuntimeException {
+        final Object user = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        final int userId = (int) ((UserInformation) user).getOtherData().get(UserDao.USR_ID);
+        attributes.put(EventDao.USR_ID, userId);
+
+        //Para convertir de String a Timestamp, ya que desde el front llega String y la base de datos acepta otro
+        // formato
+        if (attributes.containsKey("hour_event")) {
+            final String hourEventStr = (String) attributes.get("hour_event");
+
+            try {
+                final Timestamp timestamp = Timestamp.valueOf("1970-01-01 " + hourEventStr);
+                attributes.put("hour_event", timestamp);
+            } catch (final DateTimeParseException e) {
+                throw new OntimizeJEERuntimeException("Formato de hora incorrecto para hour_event: " + hourEventStr, e);
+            }
+        }
+        attributes.put("date_event", new Date(String.valueOf(attributes.get("date_event"))));
 
         return this.daoHelper.insert(this.eventDao, attributes);
     }
 
+    /*
+    public static Date getZonedDate() {
+        final ZoneId currentZone = ZoneId.of("Europe/Madrid");
+        final ZonedDateTime currentTime = ZonedDateTime.now(currentZone);
+        return Date.from(currentTime.toInstant());
+    }
+     */
+
     @Override
-    public EntityResult myEventQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException {
-        Object user = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        int userId = (int) ((UserInformation) user).getOtherData().get(UserDao.USR_ID);
+    public EntityResult myEventQuery(final Map<String, Object> keyMap, final List<String> attrList) throws OntimizeJEERuntimeException {
+        final Object user = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        final int userId = (int) ((UserInformation) user).getOtherData().get(UserDao.USR_ID);
         keyMap.put(EventDao.USR_ID, userId);
         return this.daoHelper.query(this.eventDao, keyMap, attrList);
     }

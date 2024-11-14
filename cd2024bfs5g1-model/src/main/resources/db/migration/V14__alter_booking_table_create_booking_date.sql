87d1ba828228688eeae3e2ee338e3790ca737f25
AdelaSRuizDeCortázar <sanruiade@gmail.com>
2024-11-14 10:10:50 +0100

    Modificados permisos en rol=3 (empresa) para que no accedan a myBookings

--- cd2024bfs5g1-model/src/main/resources/db/migration/V14__alter_booking_table_create_booking_date.sql
+++ cd2024bfs5g1-model/src/main/resources/db/migration/V14__alter_booking_table_create_booking_date.sql
@@ -6,3 +6,6 @@ bk_id integer not null,
 date date not null,
 primary KEY(bk_id, date),
 constraint bk_id_fk foreign key (bk_id) references booking(bk_id));
+
+UPDATE usr_role
+SET rol_json_client_permission = '{"menu":[{"attr":"admin","visible":false,"enabled":false},{"attr":"coworkingsPublic","visible":false,"enabled":false},{"attr":"login_public","visible":false,"enabled":false},{"attr":"myBookings","visible":false,"enabled":false}],"components":[{"attr":"coworkingDetail","selector":"o-form","components":[{"attr":"bookingButton","visible":false,"enabled":false},{"attr":"date","visible":false,"enabled":false},{"attr":"realCapacity","visible":false,"enabled":false}]}],"routes":[{"permissionId":"Bookings","enabled":false}]}' WHERE rol_id = 3;

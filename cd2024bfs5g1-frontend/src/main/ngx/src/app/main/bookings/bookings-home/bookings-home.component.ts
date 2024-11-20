5a8fc5fa421451372453f771afb06d871bb1e4e0
Julio Díaz <juliodiaz1792@gmail.com>
2024-11-20 14:22:00 +0100

    Booking cancel dialog service implemented

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/bookings/bookings-home/bookings-home.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/bookings/bookings-home/bookings-home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { DialogService, OTranslateService } from "ontimize-web-ngx";
 import { UtilsService } from "src/app/shared/services/utils.service";
 
 @Component({
@@ -11,10 +12,26 @@ export class BookingsHomeComponent {
   public dateStart = this.utils.dateStartFunction;
   public dateEnd = this.utils.dateEndFunction;
 
-  constructor(private router: Router, private utils: UtilsService) {}
-  
+  constructor(
+    private router: Router,
+    private utils: UtilsService,
+    private dialogService: DialogService,
+    private translate: OTranslateService
+  ) {}
+
   toCoworkingDetail(event) {
     console.log(event.row.bk_cw_id);
     this.router.navigate(["/main/coworkings/" + event.row.bk_cw_id]);
+  }
+
+  cancelBooking(evt: any) {
+    var confirmMessageTitle = this.translate.get("CANCEL_BOOKING")
+
+    if (this.dialogService) {
+      this.dialogService.confirm(
+        confirmMessageTitle,
+        ''
+      );
+    }
   }
 }

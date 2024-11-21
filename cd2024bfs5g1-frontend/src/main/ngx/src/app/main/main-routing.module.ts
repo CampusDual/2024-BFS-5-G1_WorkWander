2b05a4de6e40a8bac742313718c6d0775dee9e2f
AGonzL <agonzlopez.11@gmail.com>
2024-11-21 11:58:59 +0100

    app.menu.config.ts and main-routing.module.ts add paths

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/main-routing.module.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/main-routing.module.ts
@@ -1,33 +1,66 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AuthGuardService, PermissionsGuardService } from 'ontimize-web-ngx';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AuthGuardService, PermissionsGuardService } from "ontimize-web-ngx";
 
-import { MainComponent } from './main.component';
-import { ProfileComponent } from './profile/profile.component';
+import { MainComponent } from "./main.component";
+import { ProfileComponent } from "./profile/profile.component";
 
 export const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: MainComponent,
     canActivate: [AuthGuardService],
     canActivateChild: [PermissionsGuardService],
     children: [
-      { path: '', redirectTo: 'coworkings', pathMatch: 'full' },
-      { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-      { path: 'coworkings', loadChildren: () => import ('./coworkings/coworkings.module') .then(m =>m.CoworkingsModule)},
-      { path: 'mycoworkings', loadChildren: () => import('./my-coworkings/my-coworkings.module').then(m => m.MyCoworkingsModule) },
+      { path: "", redirectTo: "coworkings", pathMatch: "full" },
+      {
+        path: "admin",
+        loadChildren: () =>
+          import("./admin/admin.module").then((m) => m.AdminModule),
+      },
+      {
+        path: "coworkings",
+        loadChildren: () =>
+          import("./coworkings/coworkings.module").then(
+            (m) => m.CoworkingsModule
+          ),
+      },
+      {
+        path: "mycoworkings",
+        loadChildren: () =>
+          import("./my-coworkings/my-coworkings.module").then(
+            (m) => m.MyCoworkingsModule
+          ),
+      },
       //TODO
       //{ path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) },
-      { path: 'profile', component: ProfileComponent },
-      { path: 'events', loadChildren: () => import('./events/events.module').then(m => m.EventsModule) },
-      { path: 'myevents', loadChildren: () => import('./my-events/my-events.module').then(m => m.MyEventsModule) },
-      { path: 'bookings', loadChildren: () => import('./bookings/bookings.module').then(m => m.BookingsModule) }
-    ]
-  }
+      { path: "profile", component: ProfileComponent },
+      {
+        path: "events",
+        loadChildren: () =>
+          import("./events/events.module").then((m) => m.EventsModule),
+      },
+      {
+        path: "myevents",
+        loadChildren: () =>
+          import("./my-events/my-events.module").then((m) => m.MyEventsModule),
+      },
+      {
+        path: "bookings",
+        loadChildren: () =>
+          import("./bookings/bookings.module").then((m) => m.BookingsModule),
+      },
+      {
+        path: "analytics",
+        loadChildren: () =>
+          import("./analytics/analytics.module").then((m) => m.AnalyticsModule),
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MainRoutingModule { }
+export class MainRoutingModule {}
--- cd2024bfs5g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
@@ -1,20 +1,103 @@
-import { MenuRootItem } from 'ontimize-web-ngx';
+import { MenuRootItem } from "ontimize-web-ngx";
 
 export const MENU_CONFIG: MenuRootItem[] = [
-  { id: 'coworkingsHome', name: 'COWORKINGS', icon: 'home', route: '/main/coworkings' },
-  { id: 'coworkingsPublic', name: 'COWORKINGS', icon: 'home', route: '/coworkings' },
-  { id: 'coworkings', name: 'ADD_COWORKING', icon: 'people', route: '/main/coworkings/new'},
-    {id: 'admin', name: 'ADMIN', tooltip: 'ADMIN', icon: 'admin_panel_settings',
-    items: [
-      { id: 'roles', name: 'ROLES', tooltip: 'ROLES', route: '/main/admin/roles', icon: 'supervisor_account' },
-      { id: 'users', name: 'USERS', tooltip: 'USERS', route: '/main/admin/users', icon: 'person' },
-    ]
+  {
+    id: "coworkingsHome",
+    name: "COWORKINGS",
+    icon: "home",
+    route: "/main/coworkings",
   },
-  { id: 'MyCoworkings', name: 'MYCOWORKINGS', tooltip:'MyCoworkings', route: '/main/mycoworkings', icon: 'filter_list' },
+  {
+    id: "coworkingsPublic",
+    name: "COWORKINGS",
+    icon: "home",
+    route: "/coworkings",
+  },
+  {
+    id: "coworkings",
+    name: "ADD_COWORKING",
+    icon: "people",
+    route: "/main/coworkings/new",
+  },
+  {
+    id: "admin",
+    name: "ADMIN",
+    tooltip: "ADMIN",
+    icon: "admin_panel_settings",
+    items: [
+      {
+        id: "roles",
+        name: "ROLES",
+        tooltip: "ROLES",
+        route: "/main/admin/roles",
+        icon: "supervisor_account",
+      },
+      {
+        id: "users",
+        name: "USERS",
+        tooltip: "USERS",
+        route: "/main/admin/users",
+        icon: "person",
+      },
+    ],
+  },
+  {
+    id: "MyCoworkings",
+    name: "MYCOWORKINGS",
+    tooltip: "MyCoworkings",
+    route: "/main/mycoworkings",
+    icon: "filter_list",
+  },
   // Cuando se haga la funcionalidad de mostrar la tabla de eventos, habría que cambiar el route por '/main/events'.
-  { id: 'newEvents', name: 'NEW_EVENT', tooltip: 'NEW_EVENT', route: '/main/events/new', icon: 'event' },
-  { id: 'myEvents', name: 'MYEVENTS', tooltip: 'MYEVENTS', route: '/main/myevents', icon: 'event' },
-  { id: 'myBookings', name: 'MY_BOOKINGS', tooltip: 'MY_BOOKINGS', route: '/main/bookings', icon: 'bookmark' },
-  { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' },
-  { id: 'login_public', name: 'LOGIN', route: '/login', icon: 'power_settings_new', confirm: 'no' }
+  {
+    id: "newEvents",
+    name: "NEW_EVENT",
+    tooltip: "NEW_EVENT",
+    route: "/main/events/new",
+    icon: "event",
+  },
+  {
+    id: "myEvents",
+    name: "MYEVENTS",
+    tooltip: "MYEVENTS",
+    route: "/main/myevents",
+    icon: "event",
+  },
+  {
+    id: "myBookings",
+    name: "MY_BOOKINGS",
+    tooltip: "MY_BOOKINGS",
+    route: "/main/bookings",
+    icon: "bookmark",
+  },
+  {
+    id: "logout",
+    name: "LOGOUT",
+    route: "/login",
+    icon: "power_settings_new",
+    confirm: "yes",
+  },
+  {
+    id: "login_public",
+    name: "LOGIN",
+    route: "/login",
+    icon: "power_settings_new",
+    confirm: "no",
+  },
+  {
+    id: "analytics",
+    name: "ANALYTICS",
+    tooltip: "Analytics",
+    route: "/main/analytics",
+    icon: "bar_chart",
+    items: [
+      {
+        id: "occupation",
+        name: "OCCUPATION",
+        tooltip: "occupation",
+        route: "/main/analytics/occupation",
+        icon: "show_chart",
+      },
+    ],
+  },
 ];

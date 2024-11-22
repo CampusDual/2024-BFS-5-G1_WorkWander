3c876905cc889e2d3030dffc10e59f1c467dbd35
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-11-22 11:31:48 +0100

    Enlazado el detalle del evento desde Mis eventos:
    -Visualizado el detalle del evento en formato card.
    
    -Añadido el botón para regresar a mis eventos.

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/my-events/my-events-home/my-events-home.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/my-events/my-events-home/my-events-home.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-events-home',
   templateUrl: './my-events-home.component.html',
   styleUrls: ['./my-events-home.component.css']
 })
+
 export class MyEventsHomeComponent {
+constructor(private router: Router) {
+
+}
+
+  openDetail(event: any) {
+    console.log("rowData.ev_id", event);
+    this.router.navigate(["/main/myevents/" + event.row.id_event]);
+  }
 
 }

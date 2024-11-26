77e34a57823f4f2f8d379540b080db1d0309adba
Alberto Salvado Fernández <95777814+AlberSalva@users.noreply.github.com>
2024-11-26 14:07:22 +0100

    Creado Events home, ruta y botón del menú lateral

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
@@ -12,6 +12,7 @@ export const MENU_CONFIG: MenuRootItem[] = [
   },
   { id: 'MyCoworkings', name: 'MYCOWORKINGS', tooltip:'MyCoworkings', route: '/main/mycoworkings', icon: 'filter_list' },
   // Cuando se haga la funcionalidad de mostrar la tabla de eventos, habría que cambiar el route por '/main/events'.
+  { id: 'eventsHome', name: 'Eventos', icon: 'event', route: 'main/events'},
   { id: 'newEvents', name: 'NEW_EVENT', tooltip: 'NEW_EVENT', route: '/main/events/new', icon: 'event' },
   { id: 'myEvents', name: 'MYEVENTS', tooltip: 'MYEVENTS', route: '/main/myevents', icon: 'event' },
   { id: 'myBookings', name: 'MY_BOOKINGS', tooltip: 'MY_BOOKINGS', route: '/main/bookings', icon: 'bookmark' },

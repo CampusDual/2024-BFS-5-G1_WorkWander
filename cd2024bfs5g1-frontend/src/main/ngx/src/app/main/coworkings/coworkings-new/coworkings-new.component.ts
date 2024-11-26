ce72934d59f85df47d81d544bdb494e1c38c81ed
Julian Ramiro Sanchez <julian.ramiro@gmail.com>
2024-11-26 09:49:25 +0100

    Responsive map position

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings-new/coworkings-new.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings-new/coworkings-new.component.ts
@@ -37,7 +37,7 @@ export class CoworkingsNewComponent implements OnInit {
   @ViewChild("coworkingForm") coworkingForm: OFormComponent;
   @ViewChild("startDate") coworkingStartDate: ODateInputComponent;
   @ViewChild("endDate") coworkingEndDate: ODateInputComponent;
-  @ViewChild("coworkingMap") coworkingMap: OMapComponent;
+  @ViewChild("coworking_map") coworking_map: OMapComponent;
   @ViewChild('combo') combo: OComboComponent;
   @ViewChild('address') address: OTextInputComponent;
   toastService: any;
@@ -139,11 +139,9 @@ export class CoworkingsNewComponent implements OnInit {
   }
 
   //Metodos mapa usando api externa
-
   onMunicipalityChange(): void {
-    const selectedCityId = this.combo.getValue(); // Obtén el ID del municipio
+    const selectedCityId = this.combo.getValue();
 
-    // Busca el nombre de la ciudad en el dataArray del combo
     const cityObject = this.combo.dataArray.find((city) => city.id_city === selectedCityId);
     const cityName = cityObject ? cityObject.city : null;
 
@@ -151,7 +149,7 @@ export class CoworkingsNewComponent implements OnInit {
       this.getCoordinatesForCity(cityName).then((coords) => {
 
         if (coords && /^[+-]?\d+(\.\d+)?;[+-]?\d+(\.\d+)?$/.test(coords)) {
-          this.center = coords; // Asigna si el formato es válido
+          this.center = coords;
           this.zoom = 12;
         } else {
           this.showToast('Formato inválido de coordenadas: ' + coords);
@@ -166,15 +164,11 @@ export class CoworkingsNewComponent implements OnInit {
   private async getCoordinatesForCity(city: string): Promise<string | null> {
     try {
       const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(city + ', Spain')}&countrycodes=es&format=json`;
-      //console.log('Consultando API con URL:', url); // Muestra la URL completa
       const response = await this.http.get<any>(url).toPromise();
-
-      //console.log('Respuesta de la API:', response); // Imprime toda la respuesta
 
       if (response && response.length > 0) {
         this.mapLat = response[0].lat;
         this.mapLon = response[0].lon;
-        // console.log('Coordenadas encontradas:', { this.mapLat, this.mapLon });
         return `${this.mapLat};${this.mapLon}`;
       } else {
         this.showToast('No se encontraron resultados para la ciudad: ' + city);
@@ -186,30 +180,26 @@ export class CoworkingsNewComponent implements OnInit {
   }
 
   onAddressBlur(): void {
-    const selectedCityId = this.combo.getValue(); // Obtén el ID del municipio seleccionado
-    const address = this.address.getValue();  // Obtén la dirección del campo address
+    const selectedCityId = this.combo.getValue();
+    const address = this.address.getValue();
 
     if (selectedCityId && address) {
-      // Si hay municipio seleccionado y dirección, busca las coordenadas para la dirección
       this.getCoordinatesForCity(address).then((coords) => {
         if (coords) {
-          this.center = coords; // Actualiza el centro del mapa con la dirección
-          this.zoom = 18; // Ajusta el zoom
+          this.center = coords;
+          this.zoom = 18;
         } else {
           this.showToast('No se pudo encontrar la dirección. Mostrando el municipio.');
-          this.getCityCoordinatesById(selectedCityId); // Si no se encuentra, centra en el municipio
+          this.getCityCoordinatesById(selectedCityId);
         }
       });
     } else if (selectedCityId) {
-      // Si solo hay municipio seleccionado, centra en el municipio
       this.getCityCoordinatesById(selectedCityId);
     } else {
-      // Si no hay municipio ni dirección, muestra el error
       this.showToast('Por favor, selecciona un municipio o ingresa una dirección válida.');
     }
   }
 
-  // Método para obtener las coordenadas del municipio por ID
   private getCityCoordinatesById(cityId: number): void {
     const cityObject = this.combo.dataArray.find((city) => city.id_city === cityId);
     const cityName = cityObject ? cityObject.city : null;
@@ -217,8 +207,8 @@ export class CoworkingsNewComponent implements OnInit {
     if (cityName) {
       this.getCoordinatesForCity(cityName).then((coords) => {
         if (coords) {
-          this.center = coords; // Centra en el municipio
-          this.zoom = 12; // Ajusta el zoom
+          this.center = coords;
+          this.zoom = 12;
         } else {
           this.showToast('No se pudieron encontrar coordenadas para el municipio.');
         }
@@ -226,12 +216,32 @@ export class CoworkingsNewComponent implements OnInit {
     }
   }
 
-  // Método para mostrar el Toast de error
   private showToast(message: string): void {
     this.toastService.show(message, {
       classname: 'bg-danger text-light',
       delay: 5000
     });
+  }
+
+  // Metodos para controlar el draw dentro del mapa
+  ngAfterViewInit() {
+    this.getDrawLayer();
+  }
+
+  getDrawLayer() {
+    console.log(this.coworking_map.getMapService().getDrawLayer());
+  }
+
+  addDrawEvent(arg) {
+    this._eventsArray.push(arg);
+  }
+
+  get eventsArray(): Array<any> {
+    return this._eventsArray;
+  }
+
+  set eventsArray(arg: Array<any>) {
+    this._eventsArray = arg;
   }
 
 }

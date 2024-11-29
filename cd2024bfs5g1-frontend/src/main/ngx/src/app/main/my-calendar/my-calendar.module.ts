190172c6adca1814a79753433c001d6b5edf9075
JuanDAW37 <jymtr1968@gmail.com>
2024-11-29 10:41:09 +0100

    feat: My Calendar

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/my-calendar/my-calendar.module.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/my-calendar/my-calendar.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { DayPilot, DayPilotModule } from "@daypilot/daypilot-lite-angular";
 import { MyCalendarRoutingModule } from './my-calendar-routing.module';
 import { MyCalendarHomeComponent } from './my-calendar-home/my-calendar-home.component';
-
+import { OntimizeWebModule } from 'ontimize-web-ngx';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,9 @@ import { MyCalendarHomeComponent } from './my-calendar-home/my-calendar-home.com
   ],
   imports: [
     CommonModule,
-    MyCalendarRoutingModule
+    MyCalendarRoutingModule,
+    DayPilotModule,
+    OntimizeWebModule
   ]
 })
 export class MyCalendarModule { }

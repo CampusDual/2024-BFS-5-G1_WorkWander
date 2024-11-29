c2dbeaeaa159751dc32c7d973db33d3b664fd581
Julio Díaz <juliodiaz1792@gmail.com>
2024-11-29 11:09:31 +0100

    Booking-rate fixed

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/bookings/booking-rate/booking-rate.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/bookings/booking-rate/booking-rate.component.ts
@@ -1,20 +1,31 @@
-import { Component, Inject, Injector, Renderer2, ViewChild } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { DomSanitizer } from '@angular/platform-browser';
-import { ActivatedRoute, Router } from '@angular/router';
-import { OntimizeService, OSnackBarConfig, OTextareaInputComponent, OTranslateService, SnackBarService } from 'ontimize-web-ngx';
-import { Rating } from 'primeng/rating';
-import { UserInfoService } from 'src/app/shared/services/user-info.service';
+import {
+  Component,
+  Inject,
+  Injector,
+  Renderer2,
+  ViewChild,
+} from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { DomSanitizer } from "@angular/platform-browser";
+import { ActivatedRoute, Router } from "@angular/router";
+import {
+  OntimizeService,
+  OSnackBarConfig,
+  OTextareaInputComponent,
+  OTranslateService,
+  SnackBarService,
+} from "ontimize-web-ngx";
+import { Rating } from "primeng/rating";
+import { UserInfoService } from "src/app/shared/services/user-info.service";
 
 @Component({
-  selector: 'app-booking-rate',
-  templateUrl: './booking-rate.component.html',
-  styleUrls: ['./booking-rate.component.css']
+  selector: "app-booking-rate",
+  templateUrl: "./booking-rate.component.html",
+  styleUrls: ["./booking-rate.component.css"],
 })
 export class BookingRateComponent {
-
-  @ViewChild('comment') comment: OTextareaInputComponent
-  @ViewChild('ratio') ratio: Rating
+  @ViewChild("comment") comment: OTextareaInputComponent;
+  @ViewChild("ratio") ratio: Rating;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -25,13 +36,8 @@ export class BookingRateComponent {
     @Inject(OntimizeService) protected ratingService: OntimizeService,
     @Inject(UserInfoService) private userInfoService: UserInfoService,
     private injector: Injector,
-    private translate: OTranslateService,
-  ) {
-    console.log("nombre:" + data.name)
-    console.log("ratio:" + data.rate)
-    console.log("id de booking:" + data.bk_id)
-    console.log("cw_id: " + this.data.cw_id);
-  }
+    private translate: OTranslateService
+  ) {}
 
   createValoration() {
     const filter = {
@@ -39,23 +45,20 @@ export class BookingRateComponent {
       bkr_description: this.comment.getValue(),
       bkr_ratio: this.ratio.value,
       cw_id: this.data.cw_id,
-      usr_id: this.data.usr_id
+      usr_id: this.data.usr_id,
     };
 
     const conf = this.service.getDefaultServiceConfiguration("bookingsRate");
     this.service.configureService(conf);
 
     this.service.insert(filter, "bookingRate").subscribe((resp) => {
-
       this.showAvailableToast("COWORKING_VALORATION");
       this.dialogRef.close();
       window.location.reload();
-
     });
   }
 
   showAvailableToast(mensaje?: string) {
-
     const configuration: OSnackBarConfig = {
       milliseconds: 5000,
       icon: "info",

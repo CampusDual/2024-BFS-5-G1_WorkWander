e41a4fe9bb255a2cb72450db635d00d1b2387675
Ana Martínez Puga <anammpuga@gmail.com>
2024-12-02 12:22:05 +0100

    Eliminada línea de V21 y añadida a V23 junto con los permisos del rol 3 de la V22

--- cd2024bfs5g1-model/src/main/resources/db/migration/V21__update_usr_role_analytics.sql
+++ cd2024bfs5g1-model/src/main/resources/db/migration/V21__update_usr_role_analytics.sql
@@ -7,5 +7,3 @@ SET rol_json_client_permission = '{"menu":[{"attr":"admin","visible":false,"enab
 {"permissionId":"coworkings-new.route","enabled":false},{"permissionId":"analytics-occupation","enabled":false}]}'
 WHERE rol_id = 2;
 
-UPDATE usr_role
-SET rol_json_client_permission = '{"menu":[{"attr":"admin","visible":false,"enabled":false},{"attr":"coworkingsPublic","visible":false,"enabled":false},{"attr":"eventsPublic","visible":false,"enabled":false},{"attr":"login_public","visible":false,"enabled":false},{"attr":"myBookings","visible":false,"enabled":false},{"attr":"myCalendar","visible":false,"enabled":false}],"components":[{"attr":"coworkingDetail","selector":"o-form","components":[{"attr":"bookingButton","visible":false,"enabled":false},{"attr":"date","visible":false,"enabled":false},{"attr":"realCapacity","visible":false,"enabled":false}]},{"attr":"eventDetail","selector":"o-form","components":[{"attr":"bookingButton","visible":false,"enabled":false}]}],"routes":[{"permissionId":"Bookings","enabled":false},{"permissionId":"myCalendar","enabled":false}]}' WHERE rol_id = 3;

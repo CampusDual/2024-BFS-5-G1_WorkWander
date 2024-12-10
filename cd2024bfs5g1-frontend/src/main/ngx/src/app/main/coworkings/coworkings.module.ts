84283f032e2ecca96f234e620506ffc78aa5a4de
Julian Ramiro Sanchez <julian.ramiro@gmail.com>
2024-12-10 12:44:33 +0100

    Div coworking information dependencies fixed

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings.module.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/coworkings/coworkings.module.ts
@@ -5,11 +5,11 @@ import { ODateRangeInputModule, OntimizeWebModule } from 'ontimize-web-ngx';
 import { CoworkingsHomeComponent } from './coworkings-home/coworkings-home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatRadioModule } from '@angular/material/radio';
-import { ODateRangeInputModule, OntimizeWebModule } from 'ontimize-web-ngx';
 import { OMapModule } from 'ontimize-web-ngx-map';
 import { CarouselModule } from 'primeng/carousel';
 import { RatingModule } from 'primeng/rating';
 import { NearbyCoworkingComponent } from './nearby-coworking/nearby-coworking.component';
+import { NgModule } from '@angular/core';
 
 
 @NgModule({

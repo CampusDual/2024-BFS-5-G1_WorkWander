8fd379f29d3e5dd3c247afd5b41c043cdce07582
AGonzL <agonzlopez.11@gmail.com>
2025-01-08 11:05:53 +0100

    - Implemented functionality to convert dates from milliseconds to UTC format with pipe angular.

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/bookings/bookings-home/bookings-access-qr/bookings-access-qr.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/bookings/bookings-home/bookings-access-qr/bookings-access-qr.component.ts
@@ -1,15 +1,6 @@
-import {
-  Component,
-  Injector,
-  TemplateRef,
-  ViewChild,
-  ChangeDetectorRef,
-
-} from "@angular/core";
-import { DialogService, OBaseTableCellRenderer } from "ontimize-web-ngx";
+import { Component, Injector, TemplateRef, ViewChild } from "@angular/core";
+import { OBaseTableCellRenderer, OTranslateModule } from "ontimize-web-ngx";
 import { UtilsService } from "src/app/shared/services/utils.service";
-import { SafeUrl } from "@angular/platform-browser";
-import { QRCodeComponent } from "angularx-qrcode";
 import { MatDialog } from "@angular/material/dialog";
 import { QrDialogComponent } from "../qr-dialog/qr-dialog.component";
 import { OTranslateService } from "ontimize-web-ngx";
@@ -23,16 +14,11 @@ export class BookingsAccessQrComponent extends OBaseTableCellRenderer {
   @ViewChild("qrTemplate", { read: TemplateRef, static: true })
   public templateref: TemplateRef<any>;
 
-  public qrCodeData: string = "";
-  public qrCodeDownloadLink: SafeUrl = "";
-
   constructor(
     protected injector: Injector,
-    private dialogService: DialogService,
     private utilsService: UtilsService,
-    private cd: ChangeDetectorRef,
     private dialog: MatDialog,
-    private translate: OTranslateService,
+    private translate: OTranslateModule
   ) {
     super(injector);
   }
@@ -41,16 +27,8 @@ export class BookingsAccessQrComponent extends OBaseTableCellRenderer {
     this.dialog.open(QrDialogComponent, { data: rowData });
   }
 
-  onChangeURL(url: SafeUrl) {
-    this.qrCodeDownloadLink = url;
-    setTimeout(() => {
-      this.cd.detectChanges();
-    });
-  }
-
   public isBookingActive(rowData: any): boolean {
     if (!rowData) return false;
-
     const estado = this.utilsService.calculateState(rowData);
     return estado === "Pendiente" || estado === "En curso";
   }
--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/bookings/bookings-home/qr-dialog/qr-dialog.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/bookings/bookings-home/qr-dialog/qr-dialog.component.ts
@@ -1,31 +1,45 @@
+// qr-dialog.component.ts
 import { Component, Inject, ChangeDetectorRef } from "@angular/core";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { SafeUrl } from "@angular/platform-browser";
+import { DatePipe } from "@angular/common"; // Importa el DatePipe
 
 @Component({
   selector: "app-qr-dialog",
   templateUrl: "./qr-dialog.component.html",
   styleUrls: ["./qr-dialog.component.css"],
+  providers: [DatePipe], // Provee el DatePipe en el componente
 })
 export class QrDialogComponent {
-  public qrCodeData: string;
+  public qrCodeData: string = "";
   public qrCodeDownloadLink: SafeUrl = "";
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private cd: ChangeDetectorRef
+    private cd: ChangeDetectorRef,
+    private datePipe: DatePipe // Inyecta el DatePipe
   ) {
-    this.qrCodeData = JSON.stringify({
-      coworkingName: data.cw_name,
-      startDate: data.date_start,
-      endDate: data.date_end,
-    });
+    this.qrCodeData = this.buildQrData(data);
   }
 
-  public onChangeURL(url: SafeUrl) {
+  public onChangeURL(url: SafeUrl): void {
     this.qrCodeDownloadLink = url;
+
     setTimeout(() => {
       this.cd.detectChanges();
     });
+  }
+
+  private buildQrData(data: any): string {
+    const coworkingName = data.cw_name || "Nombre del Coworking";
+    const startDate = this.formatDate(new Date(data.date_start));
+    const endDate = this.formatDate(new Date(data.date_end));
+
+    return `Reserva en: ${coworkingName}\nInicio: ${startDate}\nFin: ${endDate}`;
+  }
+
+  private formatDate(date: Date): string {
+    // Utiliza el DatePipe para formatear la fecha
+    return this.datePipe.transform(date, "dd/MM/yyyy") || "";
   }
 }

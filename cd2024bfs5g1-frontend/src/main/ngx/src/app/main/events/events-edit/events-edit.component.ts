c3e5e4edcb672b9ed84b304253ccd439de0db823
AdelaSRuizDeCortázar <sanruiade@gmail.com>
2025-01-13 10:08:22 +0100

    Actualiza sin comprobaciones de plazas

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/events/events-edit/events-edit.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/events/events-edit/events-edit.component.ts
@@ -1,7 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, Injector, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
 import {
+  OFormComponent,
   OntimizeService,
+  OSnackBarConfig,
   OTranslateService,
   SnackBarService,
 } from "ontimize-web-ngx";
@@ -12,21 +14,85 @@ import {
   styleUrls: ["./events-edit.component.css"],
 })
 export class EventsEditComponent {
+
+  @ViewChild("form") form: OFormComponent;
+
   constructor(
     private router: Router,
     private translate: OTranslateService,
     private service: OntimizeService,
-    protected snackBarService: SnackBarService
-  ) {}
+    protected snackBarService: SnackBarService,
+    protected injector: Injector,
+  ) {
+    this.service = this.injector.get(OntimizeService);
+  }
+
   //Devuelve la fecha actual
   currentDate() {
     return new Date();
   }
 
-  OnInsertSuccess() {
-    const conf = this.service.getDefaultServiceConfiguration("events");
-    this.service.configureService(conf);
+  // protected configureService() {
+  //   const conf = this.service.getDefaultServiceConfiguration('events');
+  //   this.service.configureService(conf);
+  // }
 
-    
+  public onInsertSuccess(): void {
+    console.log("Test");
+    this.router.navigateByUrl("/main/myEvents")
+  }
+
+  public async save() {
+
+
+
+    //Creamos un objeto evento
+    const event = {
+      id_event: this.form.getFieldValue('id_event'),
+      name: this.form.getFieldValue('name'),
+      description: this.form.getFieldValue('description'),
+      date_event: new Date(
+        this.form.getFieldValue("date_event")
+      ).toLocaleDateString("en-CA"),
+      hour_event: this.form.getFieldValue("hour_event"),
+      duration: this.form.getFieldValue('duration'),
+      bookings: +this.form.getFieldValue('bookings'),
+      price: this.form.getFieldValue('price'),
+      locality: this.form.getFieldValue('locality'),
+      address: this.form.getFieldValue('address'),
+      image_event: this.form.getFieldValue('image_event')
+    }
+    //Llamamos a la función para actualizar, enviando el objeto
+    this.update(event);
+    this.showUpdated();
+    this.form._clearAndCloseFormAfterInsert();
+  }
+
+  public update(event: any): void {
+
+    const sqlTypes = {
+      hour_event: 12,
+      date_event: 91,
+    };
+
+    const keyMap = { id_event: this.form.getFieldValue('id_event') }
+    const conf = this.service.getDefaultServiceConfiguration('events');
+    this.service.configureService(conf);
+    this.service.update(keyMap, event, 'myEvent', sqlTypes).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  isInvalidForm(): boolean {
+    return !this.form || this.form.formGroup.invalid;
+  }
+
+  public showUpdated() {
+    const configuration: OSnackBarConfig = {
+      milliseconds: 5000,
+      icon: 'check_circle',
+      iconPosition: 'left'
+    };
+    this.snackBarService.open(this.translate.get('EVENT_UPDATE'), configuration);
   }
 }
--- cd2024bfs5g1-model/src/main/java/com/campusdual/cd2024bfs5g1/model/core/service/EventService.java
+++ cd2024bfs5g1-model/src/main/java/com/campusdual/cd2024bfs5g1/model/core/service/EventService.java
@@ -111,7 +111,26 @@ public class EventService implements IEventService {
 
     @Override
     public EntityResult myEventUpdate(final Map<String, Object> attrMap, final Map<String, Object> keyMap) {
-        return this.daoHelper.update(this.eventDao, keyMap, attrMap);
+
+        final Object user = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        final int userId = (int) ((UserInformation) user).getOtherData().get(UserDao.USR_ID);
+        attrMap.put(EventDao.USR_ID, userId);
+
+        //Para convertir de String a Timestamp, ya que desde el front llega String y la base de datos acepta otro
+        // formato
+        if (attrMap.containsKey("hour_event")) {
+            final String hourEventStr = (String) attrMap.get("hour_event");
+
+            try {
+                final Timestamp timestamp = Timestamp.valueOf("1970-01-01 " + hourEventStr);
+                attrMap.put("hour_event", timestamp);
+            } catch (final DateTimeParseException e) {
+                throw new OntimizeJEERuntimeException("Formato de hora incorrecto para hour_event: " + hourEventStr, e);
+            }
+        }
+
+
+        return this.daoHelper.update(this.eventDao, attrMap, keyMap);
     }
 }
 

c8462af6f11817a408b9751c30248f8134037fbf
AdelaSRuizDeCortázar <sanruiade@gmail.com>
2025-01-13 11:36:01 +0100

    comprobación de plazas para actualización

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/events/events-edit/events-edit.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/events/events-edit/events-edit.component.ts
@@ -32,18 +32,12 @@ export class EventsEditComponent {
     return new Date();
   }
 
-  // protected configureService() {
-  //   const conf = this.service.getDefaultServiceConfiguration('events');
-  //   this.service.configureService(conf);
-  // }
-
   public onInsertSuccess(): void {
     console.log("Test");
     this.router.navigateByUrl("/main/myEvents")
   }
 
   public async save() {
-
 
 
     //Creamos un objeto evento
@@ -55,16 +49,21 @@ export class EventsEditComponent {
         this.form.getFieldValue("date_event")
       ).toLocaleDateString("en-CA"),
       hour_event: this.form.getFieldValue("hour_event"),
-      duration: this.form.getFieldValue('duration'),
+      duration: +this.form.getFieldValue('duration'),
       bookings: +this.form.getFieldValue('bookings'),
-      price: this.form.getFieldValue('price'),
+      price: +this.form.getFieldValue('price'),
       locality: this.form.getFieldValue('locality'),
       address: this.form.getFieldValue('address'),
       image_event: this.form.getFieldValue('image_event')
     }
-    //Llamamos a la función para actualizar, enviando el objeto
-    this.update(event);
-    this.showUpdated();
+
+    if (+this.form.getFieldValue('bookings') == 0) {
+      this.plazasUsadas(event);
+    } else {
+      this.update(event);
+      console.log("updateamos no se ponen las plazas a 0");
+    }
+
     this.form._clearAndCloseFormAfterInsert();
   }
 
@@ -79,20 +78,71 @@ export class EventsEditComponent {
     const conf = this.service.getDefaultServiceConfiguration('events');
     this.service.configureService(conf);
     this.service.update(keyMap, event, 'myEvent', sqlTypes).subscribe(data => {
-      console.log(data);
+      this.showUpdatedToast('EVENT_UPDATED');
     });
   }
+
+
+
+
+  plazasUsadas(event) {
+
+    const filter = {
+      id_event: this.form.getFieldValue('id_event'),
+    };
+
+    const conf = this.service.getDefaultServiceConfiguration("bookingEvents");
+    this.service.configureService(conf);
+    const columns = [
+      "availableEventBookings",
+      "usedEventBookings",
+      "totalEventBookings",
+    ];
+
+    this.service
+      .query(filter, columns, "getEventDisponibility")
+      .subscribe((resp) => {
+
+        console.log('used: ' + resp.data['usedEventBookings']);
+        console.log('available: ' + resp.data['availableEventBookings']);
+        console.log('total: ' + resp.data['totalEventBookings']);
+        if (resp.data['usedEventBookings'] <= 0) {
+          this.update(event);
+        } else {
+          this.showWarningToast('EVENT_WITH_BOOKINGS');
+        }
+
+      });
+
+
+
+  }
+
+
+
+
 
   isInvalidForm(): boolean {
     return !this.form || this.form.formGroup.invalid;
   }
 
-  public showUpdated() {
+  showUpdatedToast(mensaje?: string) {
+    const availableMessage = mensaje;
+    const configuration: OSnackBarConfig = {
+      milliseconds: 3000,
+      icon: "info",
+      iconPosition: "left",
+    };
+    this.snackBarService.open(availableMessage, configuration);
+  }
+
+  public showWarningToast(message) {
+    const availableMessage = message;
     const configuration: OSnackBarConfig = {
       milliseconds: 5000,
-      icon: 'check_circle',
+      icon: 'warning',
       iconPosition: 'left'
     };
-    this.snackBarService.open(this.translate.get('EVENT_UPDATE'), configuration);
+    this.snackBarService.open(availableMessage, configuration);
   }
 }

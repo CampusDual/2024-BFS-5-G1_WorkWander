724d3dd35960e709fd566aa081edda75c56f8ff7
SergioSanmiguel <sergiosanmiguel4b@gmail.com>
2025-01-17 13:07:24 +0100

    paths

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/landing/landing-routing.module.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/landing/landing-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LandingComponent } from "./landing.component";
+import { LandingLegalComponent } from "./landing-legal/landing-legal.component";
+import { LandingCookiesComponent } from "./landing-cookies/landing-cookies.component";
+import { LandingPrivacyComponent } from "./landing-privacy/landing-privacy.component";
 
-const routes: Routes = [{ path: "", component: LandingComponent }];
+const routes: Routes = [
+  { path: "", component: LandingComponent },
+  { path: "legal", component: LandingLegalComponent },
+  { path: "privacy", component: LandingPrivacyComponent },
+  { path: "cookies", component: LandingCookiesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

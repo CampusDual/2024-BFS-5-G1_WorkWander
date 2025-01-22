6978461fd74b0ea5cfbfebfe64168c7b4c75374f
AGonzL <agonzlopez.11@gmail.com>
2025-01-22 14:35:19 +0100

    Add navigation to the home page when clicking on the application layout

--- cd2024bfs5g1-frontend/src/main/ngx/src/app/main/main.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/main/main.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"],
 })
 export class MainComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  goToLanding() {
+    this.router.navigate(["/landing"]);
   }
 
+  onAppLayoutClick(event: MouseEvent): void {
+    const element = event.target as HTMLElement;
+    if (element && element.tagName === "IMG") {
+      const imageSrc = (element as HTMLImageElement).src || "";
+      if (
+        imageSrc.includes("sidenav-closed.png") ||
+        imageSrc.includes("sidenav-opened.png")
+      ) {
+        this.goToLanding();
+      }
+    }
+  }
 }
--- cd2024bfs5g1-frontend/src/main/ngx/src/app/public/public-component/public-component.component.ts
+++ cd2024bfs5g1-frontend/src/main/ngx/src/app/public/public-component/public-component.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
-import { PermissionsService } from 'ontimize-web-ngx';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { PermissionsService } from "ontimize-web-ngx";
 
 @Component({
-  selector: 'app-public-component',
-  templateUrl: './public-component.component.html',
-  styleUrls: ['./public-component.component.css']
+  selector: "app-public-component",
+  templateUrl: "./public-component.component.html",
+  styleUrls: ["./public-component.component.css"],
 })
 export class PublicComponentComponent {
   hasPermission = false;
   constructor(
-    private permissionService: PermissionsService
+    private permissionService: PermissionsService,
+    private router: Router
   ) {
-    this.permissionService.getUserPermissionsAsPromise().then(x => this.hasPermission = true);
+    this.permissionService
+      .getUserPermissionsAsPromise()
+      .then((x) => (this.hasPermission = true));
+  }
+  goToLanding() {
+    this.router.navigate(["/landing"]);
+  }
+
+  onAppLayoutClick(event: MouseEvent): void {
+    const element = event.target as HTMLElement;
+    if (element && element.tagName === "IMG") {
+      const imageSrc = (element as HTMLImageElement).src || "";
+      if (
+        imageSrc.includes("sidenav-closed.png") ||
+        imageSrc.includes("sidenav-opened.png")
+      ) {
+        this.goToLanding();
+      }
+    }
   }
 }

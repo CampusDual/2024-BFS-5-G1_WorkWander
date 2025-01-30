f20715cdf61f96974f22fcf1eda816556487f208
Julian Ramiro Sanchez <julian.ramiro@gmail.com>
2025-01-30 15:02:14 +0100

    Scripts para nuevos datos - Incluyo las reseñas de las reservas

--- cd2024bfs5g1-model/src/main/resources/db/migration/V48__script_add_new_data.sql
+++ cd2024bfs5g1-model/src/main/resources/db/migration/V48__script_add_new_data.sql
@@ -13,6 +13,7 @@ DECLARE
     my_bk_id booking.bk_id%TYPE;
 
 	my_date TIMESTAMP;
+	/*  Datos de creación de usuarios */
 	num_days INT;
 	i INT;
     num_users INT := 18; -- Número de usuarios a crear
@@ -21,7 +22,26 @@ DECLARE
         'Adela', 'Juan', 'Ana', 'Hugo','Miguel', 'David', 'Julian', 'Alberto', 'Javier', 'Orlando', 'Bryan', 'Lucas', 'Alejandro', 'Tania', 'Sergio', 'Julio', 'Beatriz', 'Nataly'
     ];
 
+    /*  Creación de Reservas */
 	coworkings TEXT[] := ARRAY['Via Lactea', 'Palas Camiño', 'A Ponte', 'CoLab Zone'];  /* AÑADIR LOS COWORKINGS ELEGIDOS*/
+
+	/*  Creación de reseñas */
+    my_bk_id booking_date.bk_id%TYPE;
+    my_date booking_date.date%TYPE;
+    my_cw_id coworking.cw_id%TYPE;
+    my_user_id usr_user.usr_id%TYPE;
+    review_descriptions TEXT[] := ARRAY[
+        'Excelente experiencia, muy recomendable.',
+        'Buen lugar para trabajar, pero podría mejorar.',
+        'El ambiente es agradable y tranquilo.',
+        'Buena conexión a internet y servicios.',
+        'El personal es muy amable y servicial.'
+    ];
+    review_description TEXT;
+    review_ratio INT;
+    review_index INT := 1;
+    processed_bk_ids INT[] := '{}';
+    existing_review_count INT;
 
 BEGIN
 
@@ -88,6 +108,42 @@ BEGIN
 
         END LOOP;
 
+    END LOOP;
+
+    /*  Creamos las reseñas de las reservas */
+    -- Seleccionar todas las reservas pasadas
+    FOR my_bk_id, my_date IN
+    SELECT DISTINCT bk_id, date FROM booking_date WHERE date < CURRENT_DATE
+    LOOP
+        -- Verificar si el bk_id ya ha sido procesado
+        IF NOT my_bk_id = ANY(processed_bk_ids) THEN
+            -- Verificar si ya existe una reseña para el bk_id en la tabla booking_rate
+            SELECT COUNT(*) INTO existing_review_count FROM booking_rate WHERE id_bkr = my_bk_id;
+
+            IF existing_review_count = 0 THEN
+                -- Seleccionar el coworking y el usuario asociados a la reserva
+                SELECT bk_cw_id, bk_usr_id INTO my_cw_id, my_user_id FROM booking WHERE bk_id = my_bk_id;
+
+                -- Asignar una descripción de reseña única
+                review_description := review_descriptions[review_index];
+                review_index := review_index + 1;
+
+                IF review_index > array_length(review_descriptions, 1) THEN
+                    review_index := 1; -- Reiniciar el índice si se supera el tamaño del array
+                END IF;
+
+                -- Generar un ratio de reseña aleatorio entre 2 y 5
+                review_ratio := floor(random() * 4 + 2);
+
+                -- Insertar la reseña en la tabla Booking_rate
+                INSERT INTO booking_rate (id_bkr, bk_description, bkr_ratio, cw_id, usr_id)
+                VALUES (my_bk_id, review_description, review_ratio, my_cw_id, my_user_id);
+
+                -- Agregar el bk_id al array de procesados
+                processed_bk_ids := array_append(processed_bk_ids, my_bk_id);
+
+            END IF;
+        END IF;
     END LOOP;
 
 END $$;

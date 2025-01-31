f8901ae851e70da85cd4bdbefdb297866c63b584
Julian Ramiro Sanchez <julian.ramiro@gmail.com>
2025-01-31 13:50:20 +0100

    Script 43 - Diego con datos actualizados.

--- cd2024bfs5g1-model/src/main/resources/db/migration/V43__script_add_new_reservation_cw.sql
+++ cd2024bfs5g1-model/src/main/resources/db/migration/V43__script_add_new_reservation_cw.sql
@@ -9,19 +9,26 @@ my_date TIMESTAMP;
 	my_user_mail usr_user.usr_email%TYPE;
 	my_user_password usr_user.usr_password%TYPE;
 
-
     my_cw_id coworking.cw_id%TYPE;
     my_bk_id booking.bk_id%TYPE;
-    coworkings TEXT[] := ARRAY['Via Lactea', 'O Galo Cowork', 'A Ponte', 'CoLab Zone'];  /* AÑADIR LOS COWORKINGS ELEGIDOS*/
-    num_days INT;
+
+    coworkings TEXT[] := ARRAY['Digital Hub', 'Aitzenea', 'Buen Camino', 'Via Lactea', 'O Galo Cowork', 'A Ponte'];  /* AÑADIR LOS COWORKINGS ELEGIDOS*/
+
     i INT;
+
+    dates TIMESTAMP[] := ARRAY[
+        '2024-12-05',
+        '2024-12-31',
+        '2025-02-04',
+        '2025-02-05',
+        '2025-02-06',
+        '2025-02-08'
+    ];  -- Fechas específicas
 
 BEGIN
     -- Obtener el usr_login del array
     my_user_usr_login := 'diego';
-
     my_user_usr_name := 'Diego';
-
     -- Generar el usr_email
     my_user_mail := my_user_usr_login || '@mail.com';
 
@@ -41,37 +48,11 @@ BEGIN
             -- Seleccionar el coworking actual
         SELECT cw_id INTO my_cw_id FROM coworking WHERE cw_name = coworkings[i] LIMIT 1;
 
-        -- Fechas pasadas
-        SELECT CURRENT_DATE - INTERVAL '1 month' INTO my_date;
-
-        num_days := floor(random() * 4 + 2); -- Número de días de reserva entre 2 y 5
         INSERT INTO public.booking (bk_id, bk_usr_id, bk_cw_id, bk_state)
         VALUES (nextval('booking_bk_id_seq'::regclass), my_user, my_cw_id, true)
             RETURNING bk_id INTO my_bk_id;
 
-        FOR j IN 1..num_days LOOP
-                    INSERT INTO public.booking_date (bk_id, "date") VALUES (my_bk_id, my_date + INTERVAL '1 day' * j);
-        END LOOP;
+       INSERT INTO public.booking_date (bk_id, "date") VALUES (my_bk_id, dates[i]);
 
-                -- Fechas presentes
-        SELECT CURRENT_DATE INTO my_date;
-
-        num_days := floor(random() * 4 + 2); -- Número de días de reserva entre 2 y 5
-        INSERT INTO public.booking (bk_id, bk_usr_id, bk_cw_id, bk_state)
-        VALUES (nextval('booking_bk_id_seq'::regclass), my_user, my_cw_id, true)
-            RETURNING bk_id INTO my_bk_id;
-        FOR j IN 1..num_days LOOP
-                    INSERT INTO public.booking_date (bk_id, "date") VALUES (my_bk_id, my_date + INTERVAL '1 day' * j);
-        END LOOP;
-
-                -- Fechas futuras
-        SELECT CURRENT_DATE + INTERVAL '1 month' INTO my_date;
-        num_days := floor(random() * 4 + 2); -- Número de días de reserva entre 2 y 5
-        INSERT INTO public.booking (bk_id, bk_usr_id, bk_cw_id, bk_state)
-        VALUES (nextval('booking_bk_id_seq'::regclass), my_user, my_cw_id, true)
-            RETURNING bk_id INTO my_bk_id;
-        FOR j IN 1..num_days LOOP
-                    INSERT INTO public.booking_date (bk_id, "date") VALUES (my_bk_id, my_date + INTERVAL '1 day' * j);
-        END LOOP;
     END LOOP;
 END $$;

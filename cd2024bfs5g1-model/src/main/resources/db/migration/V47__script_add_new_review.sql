ad29fe2cf2e3c937e66ee6a0e55ee5f9bed4a009
Nataly Alvarado Lujan <nataly.al.luj@gmail.com>
2025-01-31 10:27:17 +0100

    actualización para añadir reseñas a un coworking

--- cd2024bfs5g1-model/src/main/resources/db/migration/V47__script_add_new_review.sql
+++ cd2024bfs5g1-model/src/main/resources/db/migration/V47__script_add_new_review.sql
@@ -6,18 +6,29 @@ my_bk_id booking_date.bk_id%TYPE;
     my_user_id usr_user.usr_id%TYPE;
     review_descriptions TEXT[] := ARRAY[
         'Excelente experiencia, muy recomendable.',
+                        'El coworking tiene lo justo y necesario. No destaca, pero cumple con los básicos como agua y espacio para trabajar.',
         'Buen lugar para trabajar, pero podría mejorar.',
         'El ambiente es agradable y tranquilo.',
+                         'El coworking es amplio y cómodo, pero la máquina de vending solo tenía opciones muy básicas. Me hubiera gustado más variedad.',
         'Buena conexión a internet y servicios.',
-        'El personal es muy amable y servicial.'
-    ];
+          'Tuve una experiencia regular. Las sillas eran buenas, pero el ruido de otros usuarios me distrajo bastante. Sin embargo, la pantalla extra fue útil.',
+'Un lugar inspirador para trabajar. Sillas de calidad, agua fresca siempre disponible y un ambiente colaborativo. Volveré sin duda.' ];
     review_description TEXT;
     review_ratio INT;
     review_index INT := 1;
     processed_bk_ids INT[] := '{}';
     existing_review_count INT;
+    total_reviews INT;
 
 BEGIN
+    -- Contar el número total de reseñas existentes
+SELECT COUNT(*) INTO total_reviews FROM booking_rate;
+
+-- Si ya hay 5 reseñas, salir del bloque
+IF total_reviews >= 5 THEN
+        RETURN;
+END IF;
+
     -- Seleccionar todas las reservas pasadas
 FOR my_bk_id, my_date IN
 SELECT DISTINCT bk_id, date
@@ -48,11 +59,19 @@ END IF;
                 review_ratio := floor(random() * 4 + 2);
 
                 -- Insertar la reseña en la tabla Booking_rate
-INSERT INTO booking_rate (id_bkr, bk_description, bkr_ratio, cw_id, usr_id)
+INSERT INTO booking_rate (id_bkr, bkr_description, bkr_ratio, cw_id, usr_id)
 VALUES (my_bk_id, review_description, review_ratio, my_cw_id, my_user_id);
 
 -- Agregar el bk_id al array de procesados
 processed_bk_ids := array_append(processed_bk_ids, my_bk_id);
+
+                -- Incrementar el contador de reseñas totales
+                total_reviews := total_reviews + 1;
+
+                -- Si se alcanzan las 5 reseñas, salir del bucle
+                IF total_reviews >= 5 THEN
+                    EXIT;
+END IF;
 END IF;
 END IF;
 END LOOP;
